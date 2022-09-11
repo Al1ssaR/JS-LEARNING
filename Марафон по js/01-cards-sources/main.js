27a1168a03337fdd9434eb9b172f6b4f4711ffdc
Al1ssaR <krivonosikov_aleksandr@mail.ru>
2022-09-11 15:58:02 +0300

    Cards
    Карточки , первый проект на марафоне по js

---
+++ Марафон по js/01-cards-sources/main.js
@@ -0,0 +1,12 @@
+let slides = document.querySelectorAll('.slide');
+function clearActiveClasses(){
+    slides.forEach((slide)=>{
+        slide.classList.remove('active');
+    })
+}
+for (let slide of slides){
+    slide.addEventListener('click',()=>{
+        clearActiveClasses()
+        slide.classList.add('active');
+    })
+}
