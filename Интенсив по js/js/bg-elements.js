e8b790f22bee5b04b8a623a8218f54e43039da3f
Al1ssaR <krivonosikov_aleksandr@mail.ru>
2022-09-25 12:34:43 +0300

    Интенсив (3)
    Поработал с массивом данных , отфильтровал его и вывел на страницу

--- Интенсив по js/js/bg-elements.js
+++ Интенсив по js/js/bg-elements.js
@@ -1,10 +1,7 @@
 const BgElements = ()=>{
     let elements = document.querySelectorAll('.set-bg')
-    console.log(elements)
-    for (let i = 0; i<elements.length;i++){
-        let src = elements[i].dataset.setbg
-        elements[i].style.backgroundImage = `url(${src})`
-        console.log(src)
-    }
+    elements.forEach((el) => {
+        el.style.backgroundImage = `url(${el.dataset.setbg})`
+    });
 }
 BgElements()
